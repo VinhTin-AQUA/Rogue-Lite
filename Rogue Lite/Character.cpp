294166d996855ae5a81e9e6044b38f461b85718f
Ma2thuan <100783515+Ma2thuan@users.noreply.github.com>
2022-10-27 20:44:17 +0700

    stats for main character

--- Rogue Lite/Character.cpp
+++ Rogue Lite/Character.cpp
@@ -1,1 +1,15 @@
-﻿
+#include "Character.h"
+#include "TextureManager.h"
+
+Character::Character()
+{
+	Position.SetX(0); Position.SetY(0);
+	FrameCount = 0; frameBullet = 0;
+	Width = 0, Height = 0;
+	Health = 0;
+	dx = 0; dy = 0;
+	TextureID = "";
+	_Animation = nullptr;
+	BulletID = "";
+	m_Collider = nullptr;
+}
