ed3770c6550cc50d40fa0133f0d4c2c99b6456c8
BeThan <101866497+Nhutranh@users.noreply.github.com>
2022-10-27 11:09:16 +0700

    Update Enemy_3.h
    3

--- Rogue Lite/Enemy_3.h
+++ Rogue Lite/Enemy_3.h
@@ -1,1 +1,33 @@
-﻿
+#pragma once
+#ifndef Enemy_3_h
+#define Enemy_3_h
+
+#include <SDL.h>
+#include "Enemy.h"
+#include "Bullet.h"
+
+#define  HEALTH_ENEMY 15
+
+
+class Enemy_3 : public Enemy
+{
+private:
+	int TimerBullet; // thời gian tồn tại của đạn enemy
+public:
+	Enemy_3(SDL_RendererFlip flip = SDL_FLIP_NONE); // khởi tạo enemy hoạt động trên màng hình
+
+	void DecReload();
+	int GetReload();
+	void ResetReload();
+
+
+	vector<Bullet*> GetBullets(); // lấy danh sách đạn
+	void AddBullet(string _BulletTextureID, float x_E, float y_E, int w_E, int h_E, float x_P, float y_P, int w_P, int h_P);
+	void DeleteBullet(int i);
+	void DoBullet();
+	void DrawBullet();
+	void SetPosition();
+};
+
+
+#endif // !1
