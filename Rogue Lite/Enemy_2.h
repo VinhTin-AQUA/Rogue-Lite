2fffff7f95a1568e418bfb99899924d41fad8060
BeThan <101866497+Nhutranh@users.noreply.github.com>
2022-10-27 11:08:01 +0700

    Update Enemy_2.h
    2

--- Rogue Lite/Enemy_2.h
+++ Rogue Lite/Enemy_2.h
@@ -1,1 +1,32 @@
-﻿
+#pragma once
+#ifndef Enemy_2_h
+#define Enemy_2_h
+
+#include "Enemy.h"
+#include "Bullet.h"
+
+#define  HEALTH_ENEMY 15
+
+
+class Enemy_2 : public Enemy
+{
+private:
+
+public:
+	Enemy_2(SDL_RendererFlip flip = SDL_FLIP_NONE); // khởi tạo enemy hoạt động trên màng hình
+
+	void DecReload();
+	int GetReload();
+	void ResetReload();
+
+
+	vector<Bullet*> GetBullets(); // lấy danh sách đạn
+	void AddBullet(string _BulletTextureID, float x_E, float y_E, int w_E, int h_E, float x_P, float y_P, int w_P, int h_P);
+	void DeleteBullet(int i);
+	void DoBullet();
+	void DrawBullet();
+	void SetPosition();
+};
+
+
+#endif // !1
