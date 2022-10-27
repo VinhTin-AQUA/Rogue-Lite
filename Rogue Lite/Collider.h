9196e5ef863c3650312b7901f96e3b99a8819fd7
Ma2thuan <100783515+Ma2thuan@users.noreply.github.com>
2022-10-27 20:45:21 +0700

    collider header

--- Rogue Lite/Collider.h
+++ Rogue Lite/Collider.h
@@ -1,1 +1,26 @@
-﻿
+#pragma once
+#ifndef Collider_h
+#define Collider_h
+
+#include <SDL.h>
+#include "Transform.h"
+
+class Collider // tạo 1 hình chữ nhật bao quanh đối tượng
+{
+private:
+	SDL_Rect m_Box; // hình chữ nhật bao quanh đối tượng (đạn, player, enymies)
+
+	Transform LastPosition; // 5;6 66;7
+
+public:
+	inline SDL_Rect Get() { return m_Box; }
+	
+	void SetLastPositionX(float x);
+	void SetLastPositionY(float y);
+
+	Transform GetLastPosition();
+	void Set(float x, float y, int w, int h);
+
+};
+
+#endif
